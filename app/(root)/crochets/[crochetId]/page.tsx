d6220bc1e084df09379423ce9d9dd0e80c3973a7
Ozdal Dogru <ozdaldogru@gmail.com>
2025-02-01 02:55:32 -0500

    refactor CrochetDetails component for improved parameter handling and code readability

--- app/(root)/crochets/[crochetId]/page.tsx
+++ app/(root)/crochets/[crochetId]/page.tsx
@@ -8,38 +8,35 @@ interface CrochetCardProps {
   crochet: CrochetType;
 }
 
-const CrochetDetails = async ({ params }: { params: { crochetId: string } }) => {
-  const crochetDetails = await getCrochetDetails(params.crochetId);
+const CrochetDetails = async ({ params }: { params: Promise<{ crochetId: string }> }) => {
+  const { crochetId } = await params;
+  const crochetDetails = await getCrochetDetails(crochetId);
 
   return (
-    <>
-      <div className="flex flex-wrap items-center flex flex-col gap-5 py-5 px-5">
-        <div className="flex justify-center md:gap-2 text-[24px] max-[431px]:text-[20px]">
-          <CrochetTitle crochetInfo={crochetDetails} />
+    <div className="flex flex-wrap items-center flex flex-col gap-5 py-5 px-5">
+      <div className="flex justify-center md:gap-2 text-[24px] max-[431px]:text-[20px]">
+        <CrochetTitle crochetInfo={crochetDetails} />
+      </div>
+
+      <div className="max-w-[1290px] w-full h-full flex flex-col gap-2">
+        <CrochetGallery crochetMedia={crochetDetails.media} />
+        <br />
+
+        <div className="text-center flex flex-col gap-2">
+          <h2 className="text-[35px] max-[431px]:text-[20px] text-bold text-center py-2 border-b-2 border-[#E5E5E5] py-5">
+            Description
+          </h2>
+          <CrochetDescription crochetInfo={crochetDetails} />
         </div>
 
-        <div className="max-w-[1290px] w-full h-full flex flex-col gap-2">
-          <CrochetGallery crochetMedia={crochetDetails.media} />
-          <br />
-
-          <div className="text-center flex flex-col gap-2">
-            <h2 className="text-[35px] max-[431px]:text-[20px] text-bold text-center py-2 border-b-2 border-[#E5E5E5] py-5">
-              Description
-            </h2>
-            <CrochetDescription crochetInfo={crochetDetails} />
-          </div>
-
-          <div className="flex justify-center md:gap-2 text-[24px] max-[431px]:text-[20px]">
-            <p className="text-body-bold text-[#8F95A0] max-[431px]:text-[15px]">
-              ${crochetDetails.price.toLocaleString()}
-            </p>
-          </div>
+        <div className="flex justify-center md:gap-2 text-[24px] max-[431px]:text-[20px]">
+          <p className="text-body-bold text-[#8F95A0] max-[431px]:text-[15px]">
+            ${crochetDetails.price.toLocaleString()}
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
-
-export const dynamic = "force-dynamic";
 
 export default CrochetDetails;
